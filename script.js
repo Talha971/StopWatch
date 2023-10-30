57cab67f5ac0ab544df2d65fb41fcfa3a4b49038
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-10-31 00:36:56 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,17 +6,39 @@ var getMin = document.getElementById("min");
 var getSec = document.getElementById("sec");
 var getMSec = document.getElementById("msec");
 
-setInterval(function () {
-  miliseconds++;
-  getMSec.innerHTML = miliseconds;
-  if (miliseconds == 100) {
-    seconds++;
-    getSec.innerHTML = seconds;
-    miliseconds = 0;
-    if (seconds == 60) {
+var a = document.getElementById("str");
+var b = document.getElementById("stp");
+var c = document.getElementById("rst");
+
+var interval;
+
+a.addEventListener("click", function () {
+  interval = setInterval(function () {
+    miliseconds++;
+    getMSec.innerHTML = miliseconds;
+    if (miliseconds == 100) {
+      seconds++;
+      getSec.innerHTML = seconds;
+      miliseconds = 0;
+    } else if (seconds == 60) {
       seconds = 0;
       minutes++;
       getMin.innerHTML = minutes;
     }
-  }
-}, 10);
+  }, 10);
+  document.getElementById("str").disabled = true;
+});
+
+b.addEventListener("click", function () {
+  clearInterval(interval);
+  document.getElementById("str").disabled = false;
+});
+
+c.addEventListener("click", () => {
+  minutes = 0;
+  seconds = 0;
+  miliseconds = 0;
+  getMSec.innerHTML = 0;
+  getMin.innerHTML = 0;
+  getSec.innerHTML = 0;
+});
